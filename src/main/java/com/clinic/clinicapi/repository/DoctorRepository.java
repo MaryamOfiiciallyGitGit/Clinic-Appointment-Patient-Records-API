be4ec880a2af2bd1ada886d54a702e06055e1673
maryamGit <maryamofficially24@gmail.com>
2026-06-23 14:44:36 +0400

    add

--- src/main/java/com/clinic/clinicapi/repository/DoctorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.clinic.clinicapi.repository;
-
-import com.clinic.clinicapi.entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// This repository manages Doctor data in the database
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
